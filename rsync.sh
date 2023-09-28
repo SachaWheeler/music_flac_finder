97fb98ed06d98fdec74d312c21347da6fd3cfa99
Sacha Wheeler <sacha@sachawheeler.com>
2023-09-28 12:09:54 +0100

    remove irrelavent files

--- rsync.sh
+++ rsync.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
-#rsync -zav  --prune-empty-dirs --dry-run /home/sacha/Downloads/ happy@happy.local:/Volumes/backup/new_music/ --include "*/" --include="*.mp3" --include="*.flac" --include="*.ogg" --exclude="*"
-rsync -zav --remove-source-files --prune-empty-dirs /home/sacha/Downloads/ happy@happy.local:/Volumes/backup/new_music/ --include "*/" --include="*.mp3" --include="*.flac" --include="*.ogg" --exclude="*"
+cd ~/Downloads
+# ls */* | grep -v mp3 | grep -v flac | grep -v part | less
+
+
+find . -type f -name "*.cue" -delete
+find . -type f -name "*.CUE" -delete
+find . -type f -name "*.log" -delete
+find . -type f -name "*.jpg" -delete
+find . -type f -name "*.jpeg" -delete
+find . -type f -name "*.JPG" -delete
+find . -type f -name "*.txt" -delete
+find . -type f -name "*.url" -delete
+find . -type f -name "*.md5" -delete
+find . -type f -name "*.docx" -delete
+find . -type f -name "*.m3u" -delete
+find . -type f -name "*.ffp" -delete
+find . -type f -name "*.nfo" -delete
+find . -type f -name "*.png" -delete
+find . -type f -name "*.gif" -delete
+find . -type f -name "*.pdf" -delete
+find . -type f -name "Thumbs.db" -delete
+
+rmdir */*/*/*
+rmdir */*/*
+rmdir */*
+rmdir *
+
+rsync -zav --remove-source-files --prune-empty-dirs /home/sacha/Downloads/ happy@happy.local:/Volumes/backup/new_music/ --include "*/" --include="*.m4a" --include="*.mp3" --include="*.flac" --include="*.ogg" --exclude="*"
 
