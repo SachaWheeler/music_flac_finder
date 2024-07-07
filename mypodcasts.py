e27b1a299fa362fa7aa6eb189552be949c31ebff
Sacha Wheeler <sacha@sachawheeler.com>
2024-07-07 08:48:08 +0100

    podcast feeds

--- mypodcasts.py
+++ mypodcasts.py
@@ -4,11 +4,18 @@ import getpodcast
 
 opt = getpodcast.options(
     date_from='2024-01-01',
-    root_dir='./podcast')
+    root_dir='/moshpit/podcast')
+
+# get podcast feed url here
+# https://www.labnol.org/podcast/
 
 podcasts = {
         # "Ross_and_Carrie": "https://podcasts.apple.com/gb/podcast/oh-no-ross-and-carrie/id425328515",
-    "SGU": "https://feed.theskepticsguide.org/feed/sgu"
+    "ONRAC": "https://feeds.simplecast.com/ftB6Gihc",
+    "HDTGM": "https://feeds.simplecast.com/Ao0C24M8",
+    "Problem_squared": "https://anchor.fm/s/eb804d78/podcast/rss",
+    "RobWords": "https://audioboom.com/channels/5128892.rss",
+    #"SGU": "https://feed.theskepticsguide.org/feed/sgu"
 }
 
 getpodcast.getpodcast(podcasts, opt)
