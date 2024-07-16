6e5d512382705be2e931f7afbd683092edcdaa00
Sacha Wheeler <sacha@sachawheeler.com>
2024-07-16 09:56:16 +0100

    clean up

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /moshpit/Music
-find ./incoming/ -name "*.flac" -exec ffmpeg -n -i {} -ab 320k -map_metadata 0 -id3v2_version 3 {}.mp3 \;
-find ./incoming/ -name "*.mp3" -exec mv {} "Automatically Add to Music.localized"  \;
-find ./incoming/ -name "*.m4a" -exec mv {} "Automatically Add to Music.localized"  \;
-mv incoming/* removed/
