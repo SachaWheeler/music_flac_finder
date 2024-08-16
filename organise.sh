1da53f97e104bc96d67cdb69d551004cd4229afe
Sacha Wheeler <sacha@sachawheeler.com>
2024-08-16 08:50:54 +0100

    covert flacs and move m p3s, tv shows and movies top the correct dirs

--- organise.sh
+++ organise.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
-cd ~/Downloads
-# ls */* | grep -v mp3 | grep -v flac | grep -v part | less
+MOUNT_POINT="/moshpit"
+INCOMING_DIR="/home/sacha/incoming_music"
 
-HOST='happy.local'
-USER='happy'
-# INCOMING_DIR='/Users/happy/Music/Music2023/Media.localized/incoming/'
-INCOMING_DIR='/Volumes/moshpit/Music/incoming/'
+MUSIC_DEST_DIR="$MOUNT_POINT/Music/Automatically Add to Music.localized/"
+TV_DEST_DIR="$MOUNT_POINT/tv/"
+MOVIE_DEST_DIR="$MOUNT_POINT/movies/"
 
-rsync -zav --remove-source-files --prune-empty-dirs /home/sacha/Downloads/ $USER@$HOST:$INCOMING_DIR --include "*/" --include="*.m4a" --include="*.mp3" --include="*.flac" --include="*.ogg" --exclude="*"
 
-ssh $USER@$HOST<<END_SSH
-cd $INCOMING_DIR
-pwd
-ls
+if mount | grep -q "on $MOUNT_POINT type fuse.sshfs"; then
+    echo "Remote disk is mounted at $MOUNT_POINT."
+else
+    echo "Remote disk is NOT mounted at $MOUNT_POINT."
+    sshfs -o allow_other happy@happy.local:/Volumes/moshpit $MOUNT_POINT
+fi
 
-find . -name "*.flac" -exec ffmpeg -n -i {} -ab 320k -map_metadata 0 -id3v2_version 3 {}.mp3 \;
+# convert flacs to mp3
+find "$INCOMING_DIR" -name "*.flac" -exec ffmpeg -n -i {} -ab 320k -map_metadata 0 -id3v2_version 3 {}.mp3 \;
+find "$INCOMING_DIR" -type f -name "*.flac" -exec rm {} \;
 
-mv * ../"Automatically Add to Music.localized"
+# move mp3s to MUSIC_DEST_DIR
+find "$INCOMING_DIR" -type f -name "*.mp3" -exec cp --no-preserve=mode,ownership {} "$MUSIC_DEST_DIR" \;
+find "$INCOMING_DIR" -type f -name "*.mp3" -exec rm {} \;
+
+# move TV shows
+find "$INCOMING_DIR" -type f -regextype posix-egrep -regex '.*S[0-9]{2}E[0-9]{2}.*\.(mp4|mkv|avi|mov|flv|wmv|m4v)' -exec mv {} "$TV_DEST_DIR" \;
+
+# and any other video files
+find "$INCOMING_DIR" -type f -regextype posix-egrep -regex '.*\.(mp4|mkv|avi|mov|flv|wmv|m4v)' -exec mv {} "$MOVIE_DEST_DIR" \;
 
 echo 'Done'
-END_SSH
