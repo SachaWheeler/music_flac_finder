958f05c58e0f98a0b254adf656b575d6afb0e110
Sacha Wheeler <sacha@sachawheeler.com>
2025-01-26 11:17:54 +0000

    stats

---
+++ stats.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TOTAL=$(wc -l bitrates.txt)
+echo "total files: ${TOTAL}"
+
+NOT_FOUND=$(grep "not found" bitrates.txt | wc -l)
+echo "no tag data: ${NOT_FOUND}"
+
+grep -v "not found" bitrates.txt | cut -d":" -f2 | sort | uniq -c | sort -rn
